206ca6e166200c93b8e84eed4f356d917f45e4c3
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2024-07-29 10:59:46 -0300

    Update string.s

--- string.s
+++ string.s
@@ -1,5 +1,28 @@
 .section .data
-a: .quad 0
+string: .byte "1", "2", "3"
+resultado: .quad 0
+
 
 .section .text
-global _start
+.globl _start
+
+_start:
+
+movb $0, %al
+movb $0, %bl
+movb $0, %cl
+movb string(, %al,1), %bl
+movb string(, %al,2), %cl
+
+mulb $10, %cl
+addb %cl, %bl
+
+movb string(, %al,3), %cl
+
+mulb $100, %cl
+addb %cl, %bl
+
+movzbq %bl, %rdi
+
+movq $60, %rax
+syscall
